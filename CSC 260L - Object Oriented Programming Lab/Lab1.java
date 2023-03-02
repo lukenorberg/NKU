60712aa380ee04009ec9d016616703294bac0d49
lukenorberg <106898337+lukenorberg@users.noreply.github.com>
2023-03-01 22:28:12 -0500

    Add files via upload

---
+++ CSC 260L - Object Oriented Programming Lab/Lab1.java
@@ -0,0 +1,29 @@
+/*
+    Author: Luke Norberg
+    Course: CSC260L - 002
+    Date: 01/17/2023
+    Assignment #1
+    Instructor: Brian Sauer
+ */
+
+public class Lab1 {
+    public static void main(String[] args){
+        String name = "Luke";
+        int age = 18;
+        String messageOfTheDay = "Information is not knowledge, knowledge is not wisdom!";
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old" + "\n\n");
+        System.out.println("The message of the day is " + messageOfTheDay + "\n");
+        System.out.println("Done");
+    }
+}
+
+/*
+Hello Luke
+You are 18 years old
+
+
+The message of the day is Information is not knowledge, knowledge is not wisdom!
+
+Done
+ */
